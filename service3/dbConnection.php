3d14a634d9b2f948bae1024b87967f2b253843ff
almeidatarcisio <tarcisiocomp@gmail.com>
2023-09-05 13:58:02 -0300

    atualizado para servidor railway

--- service3/dbConnection.php
+++ service3/dbConnection.php
@@ -3,10 +3,10 @@ $ambiente = true;
 
 if ($ambiente) { // Ambiente de Produção
 	
-	$HostName = "wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-	$HostUser = "tm4ab7xzn205n7l1";
-	$HostPass = "p0ythsi7ylyy2pah";
-	$DatabaseName = "bcxc6b0aepzciccy";
+	$HostName = "containers-us-west-126.railway.app";
+	$HostUser = "root";
+	$HostPass = "CPiVVuGD2hS2BVMyWkER";
+	$DatabaseName = "railway";
 	
 } else { // Ambiente de Desenvolvimento
 
