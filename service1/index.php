0c54bd4bec94a5f0e19660c985c1b56100ff5a5e
almeidatarcisio <tarcisiocomp@gmail.com>
2024-08-30 00:36:55 -0300

    atualização getUsuarios

--- service1/index.php
+++ service1/index.php
@@ -1,28 +1,36 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json charset=utf-8');
 
 $response = array();
 $response["erro"] = true;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	
+
 	include 'dbConnection.php';
-	
-	$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+
+	$conn = new mysqli($HostName, $HostUser,
+					$HostPass, $DatabaseName);
+
 	mysqli_set_charset($conn, "utf8");
-	
+
 	if ($conn->connect_error) { 
+
 		die("Não conectou....: " . $conn->connect_error);
 	}
-	
-	$login = $conn->real_escape_string($_POST['login']);
-	$senha = $conn->real_escape_string($_POST['senha']);
-	
-	$sql = "SELECT * FROM usuario WHERE login = '$login' AND senha = '$senha'";
+
+	$login = "'".$_POST['login']."'";
+	$senha = "'".$_POST['senha']."'";
+
+	$sql = "SELECT * FROM usuario WHERE login = $login
+					 AND senha = $senha";
+
 	$result = $conn->query($sql);
-	
+
+//	print_r($result);
+
 	if ($result->num_rows > 0) {
-		$registro = $result->fetch_assoc();
+
+		$registro = mysqli_fetch_array($result);
 
 		$response["registros"] = $result->num_rows;
 		$response["erro"] = false;
@@ -30,12 +38,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$response["senha"] = $registro['senha'];
 		$response["tpusuario"] = $registro['fk_id_tpusuario'];
 		$response["datainclusao"] = $registro['datainclusao'];
-	} else {
-		$response["mensagem"] = "Usuário e/ou senha inválidos!";
-	}
-	
-	$conn->close();
-}
 
+	} else {
+
+		$response["mensagem"] = "Usuário e/ou senha inválidos!";
+
+	}
+
+	$conn->close();
+
+}
 echo json_encode($response);
 ?>
