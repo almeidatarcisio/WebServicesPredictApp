76e6a7f535b5cba5f12cdf652a7992143ac5123f
almeidatarcisio <tarcisiocomp@gmail.com>
2024-11-25 12:41:03 -0300

    ajuste CORS em service2 e service3

--- service2/index.php
+++ service2/index.php
@@ -1,4 +1,8 @@
 <?php
+// Configura os cabeçalhos para permitir requisições de outras origens
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json charset=utf-8');
 
 $response = array();
